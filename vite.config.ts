0abea7c9e68e77400807d45cc3d92babe6d55edc
Lucien <lucien.neuhoff@protonmail.com>
2024-09-08 17:27:30 +0200

    feat: :sparkles: initialized project

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import type { KIT_ROUTES } from '$lib/ROUTES';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { kitRoutes } from 'vite-plugin-kit-routes';
+
+export default defineConfig({
+	plugins: [kitRoutes<KIT_ROUTES>(), sveltekit()]
+});
