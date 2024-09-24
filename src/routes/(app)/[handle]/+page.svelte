8936968e016a016ef6c42cb8b8be11fb18d37ba2
Lucien <lucien.neuhoff@protonmail.com>
2024-09-24 16:09:28 +0200

    feat: :sparkles: added private profile lock on profile avatar

--- src/routes/(app)/[handle]/+page.svelte
+++ src/routes/(app)/[handle]/+page.svelte
@@ -4,9 +4,14 @@
 	import { ProfileAvatar } from '@/profile/avatar';
 	import type { PageData } from './$types';
 	import PostCardSkeleton from '@/post/post-card-skeleton.svelte';
+	import { Lock } from 'lucide-svelte';
+	import * as Tooltip from '&/tooltip';
+	import { getAuthState } from '@/auth/auth-state.svelte';
 
 	const { data } = $props();
 	const { profile, postsPromise }: PageData = data;
+
+	const authState = getAuthState();
 </script>
 
 <div class="flex flex-col">
@@ -20,7 +25,19 @@
 		</div>
 	{:then { profile, posts }} -->
 	<div class="flex">
-		<ProfileAvatar {profile} size="lg" />
+		<div class="relative">
+			<ProfileAvatar {profile} size="lg" />
+			{#if profile.privacySettings?.private}
+				<Tooltip.Root>
+					<Tooltip.Trigger class="absolute bottom-2 right-2 rounded-full border bg-background">
+						<Lock class="h-8 w-8 p-2" />
+					</Tooltip.Trigger>
+					<Tooltip.Content>
+						{profile.id === authState.user?.id ? 'Your' : `${profile.displayName}'s`} profile is private
+					</Tooltip.Content>
+				</Tooltip.Root>
+			{/if}
+		</div>
 		<div class="flex flex-col gap-1 py-6 pl-5">
 			<h1 class="text-4xl">{profile.displayName}</h1>
 			<p class="text-muted-foreground">@{profile.handle}</p>
