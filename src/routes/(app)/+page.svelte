d56a57cb2fc301b9e5399467a31a5579497f8b53
Lucien <lucien.neuhoff@protonmail.com>
2024-09-26 20:39:29 +0200

    feat: :sparkles: smooth scroll home page post list

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -3,14 +3,39 @@
 	import type { PageData } from './$types';
 	import CreatePostForm from './create-post-form.svelte';
 	import PostCardSkeleton from '@/post/post-card-skeleton.svelte';
+	import { Button } from '&/button';
+	import { ArrowUp } from 'lucide-svelte';
 
 	const { data } = $props();
 	const { postsPromise, createPostForm }: PageData = data;
+
+	let el: HTMLElement | undefined = $state();
+	let scroll: number = $state(0);
+	const handleScroll = (event: Event) => {
+		const target = event.target as HTMLDivElement;
+		scroll = target.scrollTop;
+	};
+
+	const resetScroll = () => {
+		if (!el) return;
+		el.scrollTo({ top: 0, behavior: 'smooth' });
+	};
 </script>
 
-<CreatePostForm data={createPostForm} />
+<div
+	class="realtive w-full space-y-4 overflow-y-scroll py-4"
+	bind:this={el}
+	onscroll={handleScroll}
+>
+	<CreatePostForm data={createPostForm} />
 
-<div class="w-full space-y-4 py-4">
+	{#if scroll > 200}
+		<Button class="absolute left-0 right-0 top-3 z-10 mx-auto w-fit" onclick={resetScroll}>
+			<ArrowUp />
+			<span>Back To Top</span>
+		</Button>
+	{/if}
+
 	{#await postsPromise}
 		<!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
 		{#each { length: 5 } as _}
