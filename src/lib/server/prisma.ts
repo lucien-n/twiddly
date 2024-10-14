12c274d1f2593b2571f15a9248d47f8a2c162c8b
lucien-n <contact@lucienn.dev>
2024-10-14 16:05:12 +0200

    feat: :lock: isolate db and mailjet in tests

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient(
+	process.env.NODE_ENV === 'test'
+		? {
+				datasourceUrl: 'dont-touch-my-database'
+			}
+		: undefined
+);
 
 // todo: see — https://pris.ly/d/extensions
 prisma.$use(async (params, next) => {
