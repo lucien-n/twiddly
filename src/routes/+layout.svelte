5d145581f48555a0067c41df1ad4d9e2c597d458
lucien-n <contact@lucienn.dev>
2024-10-14 16:26:04 +0200

    feat: :sparkles: navigation loading bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,22 @@
 <script>
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from 'svelte-sonner';
+	import { navigating } from '$app/stores';
 	import '../app.css';
+	import { fly } from 'svelte/transition';
 
 	const { children } = $props();
 </script>
 
 <Toaster richColors />
 <ModeWatcher />
+
+{#if $navigating}
+	<div class="absolute z-[9] w-full" transition:fly={{ y: -100, duration: 100 }}>
+		<div class="h-1.5 w-full overflow-hidden bg-secondary">
+			<div class="progress left-right h-full w-full bg-secondary-foreground"></div>
+		</div>
+	</div>
+{/if}
 
 {@render children()}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,19 @@ const config: Config = {
 			},
 			fontFamily: {
 				sans: [...fontFamily.sans]
+			},
+			animation: {
+				progress: 'progress 1s infinite linear'
+			},
+			keyframes: {
+				progress: {
+					'0%': { transform: ' translateX(0) scaleX(0)' },
+					'40%': { transform: 'translateX(0) scaleX(0.4)' },
+					'100%': { transform: 'translateX(100%) scaleX(0.5)' }
+				}
+			},
+			transformOrigin: {
+				'left-right': '0% 50%'
 			}
 		}
 	}
