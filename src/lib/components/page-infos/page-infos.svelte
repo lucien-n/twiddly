289d19fee953bf9aebf487c839f8b845397f58e4
Lucien <lucien.neuhoff@protonmail.com>
2024-10-26 14:06:24 +0200

    fix: :bug: fixed pages infos

--- src/lib/components/page-infos/page-infos.svelte
+++ src/lib/components/page-infos/page-infos.svelte
@@ -5,14 +5,14 @@
 </script>
 
 <svelte:head>
-	<title>{infos.title}</title>
-	<meta name="author" content={infos.author ?? 'Twiddly'} />
+	<title>{infos?.title ?? 'Twiddly'}</title>
+	<meta name="author" content={infos?.author ?? 'Twiddly'} />
 	<meta
 		name="description"
-		content={infos.description ?? 'Twiddly - Share and connect with your peers'}
+		content={infos?.description ?? 'Twiddly - Share and connect with your peers'}
 	/>
-	<meta property="og:title" content={infos.title} />
-	<meta property="og:image" content={infos.image ?? `${$page.url.origin}/favicon.svg`} />
-	<meta property="og:image:type" content={infos.imageType ?? 'image/svg'} />
+	<meta property="og:title" content={infos?.title ?? 'Twiddly'} />
+	<meta property="og:image" content={infos?.image ?? `${$page.url.origin}/favicon.svg`} />
+	<meta property="og:image:type" content={infos?.imageType ?? 'image/svg'} />
 	<meta property="og:url" content={$page.url.href} />
 </svelte:head>
