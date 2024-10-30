f72fd9c643cd21855484ab1c214c9a88fbdc90de
Lucien <lucien.neuhoff@protonmail.com>
2024-10-30 23:25:23 +0100

    feat: :bug: twiddle link only on twiddle body

--- src/lib/sections/twiddle/twiddle-card.svelte
+++ src/lib/sections/twiddle/twiddle-card.svelte
@@ -1,31 +1,16 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { route } from '$lib/ROUTES';
 	import { formatDate } from '$lib/utils/date';
 	import { Button } from '&/ui/button';
 	import * as Card from '&/ui/card';
 	import { ProfileAvatar } from '@/profile';
-	import TwiddleFooter from './twiddle-footer.svelte';
 	import { getTwiddleState } from './state/twiddle-state.svelte';
+	import TwiddleFooter from './twiddle-footer.svelte';
 
 	const twiddle = getTwiddleState();
-
-	const twiddleHref = $derived(
-		route('/[handle]/[twiddleId]', {
-			handle: twiddle.data.author.handle,
-			twiddleId: twiddle.data.id
-		})
-	);
-
-	const redirectToTwiddle = () => {
-		goto(twiddleHref);
-	};
 </script>
 
-<Card.Root
-	class="mx-auto transition-shadow duration-300 hover:cursor-pointer hover:shadow-lg"
-	onclick={redirectToTwiddle}
->
+<Card.Root class="mx-auto transition-shadow duration-300 hover:shadow-lg">
 	<Card.Header>
 		<div class="flex items-center space-x-4">
 			<a href={route('/[handle]', { handle: twiddle.data.author.handle })}>
@@ -44,16 +29,23 @@
 		</div>
 	</Card.Header>
 	<Card.Content>
-		<p class="mb-2 text-foreground">{twiddle.data.content}</p>
-		<p class="flex gap-1 text-sm text-muted-foreground">
-			<a href={twiddleHref} aria-label={twiddle.data.content}>
-				{formatDate(twiddle.data.createdAt)}
-			</a>
-			{#if twiddle.data.isEdited}
-				<span>·</span>
-				<span>Edited</span>
-			{/if}
-		</p>
+		<a
+			href={route('/[handle]/[twiddleId]', {
+				handle: twiddle.data.author.handle,
+				twiddleId: twiddle.data.id
+			})}
+		>
+			<p class="text-foreground">{twiddle.data.content}</p>
+			<p class="mt-2 flex gap-1 text-sm text-muted-foreground">
+				<span>
+					{formatDate(twiddle.data.createdAt)}
+				</span>
+				{#if twiddle.data.isEdited}
+					<span>·</span>
+					<span>Edited</span>
+				{/if}
+			</p>
+		</a>
 	</Card.Content>
 	<Card.Footer class="justify-between py-3">
 		<TwiddleFooter />
