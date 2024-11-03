03d64b5098c5afd0debc5fa9645b9e0e92715548
lucien-n <contact@lucienn.dev>
2024-11-03 17:36:58 +0100

    wip: maintenance mode hover card

--- src/routes/maintenance-mode-alert.svelte
+++ src/routes/maintenance-mode-alert.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { MaintenanceMode } from '@prisma/client';
-	import * as Alert from '&/ui/alert';
-	import { TriangleAlert } from 'lucide-svelte';
+	import * as HoverCard from '&/ui/hover-card';
+	import { TriangleAlert, MessageCircleQuestion } from 'lucide-svelte';
+	import { Button, buttonVariants } from '&/ui/button';
 
 	interface Props {
 		maintenanceMode: MaintenanceMode | undefined;
@@ -21,11 +22,21 @@
 </script>
 
 {#if maintenanceMode && maintenanceMode !== MaintenanceMode.OPEN}
-	<Alert.Root class="absolute right-2 top-2 z-10 w-fit">
-		<TriangleAlert class="size-4" />
-		<Alert.Title>Maintenance</Alert.Title>
-		<Alert.Description>
-			Twiddly is under restricted access and only accessible to {accessibleTo}
-		</Alert.Description>
-	</Alert.Root>
+	<HoverCard.Root onOpenChange={console.log}>
+		<HoverCard.Trigger
+			class={buttonVariants({
+				size: 'icon',
+				variant: 'secondary',
+				class: 'absolute right-2 top-2 z-50'
+			})}
+		>
+			<TriangleAlert />
+		</HoverCard.Trigger>
+		<HoverCard.Content>
+			<p class="mb-1 font-medium leading-none tracking-tight">Maintenance</p>
+			<p class="text-sm [&_p]:leading-relaxed">
+				Twiddly is under restricted access and only accessible to {accessibleTo}
+			</p>
+		</HoverCard.Content>
+	</HoverCard.Root>
 {/if}
