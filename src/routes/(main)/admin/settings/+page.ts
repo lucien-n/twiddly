74b74282c641d5026498b9ad3b2540d4a35ae55c
lucien-n <contact@lucienn.dev>
2024-11-05 15:05:49 +0100

    fix: :bug: admin page infos not passing data

--- src/routes/(main)/admin/settings/+page.ts
+++ src/routes/(main)/admin/settings/+page.ts
@@ -1,6 +1,7 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async () => ({
+export const load: PageLoad = async ({ data }) => ({
+	...data,
 	infos: {
 		title: 'Admin - Settings'
 	}
