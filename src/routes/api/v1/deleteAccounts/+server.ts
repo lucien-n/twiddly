a1852b1b53024236fdd8624f1f81e10fa58aacf4
lucien-n <contact@lucienn.dev>
2024-11-06 13:07:25 +0100

    fix: :bug: renamed cron secret env var

--- src/routes/api/v1/deleteAccounts/+server.ts
+++ src/routes/api/v1/deleteAccounts/+server.ts
@@ -1,11 +1,11 @@
-import { ACCOUNT_DELETION_CRON_SECRET } from '$env/static/private';
+import { CRON_SECRET } from '$env/static/private';
 import { prisma } from '$lib/server/prisma';
 import { error, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async (event) => {
 	const bearer = (event.request.headers.get('authorization') || '').split('Bearer ').at(1);
 
-	if (!bearer || bearer != ACCOUNT_DELETION_CRON_SECRET) {
+	if (!bearer || bearer != CRON_SECRET) {
 		return error(401);
 	}
 
