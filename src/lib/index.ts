cb0b70982a2f07bbb1efe0e4e8cdef4b893324e0
lucien-n <contact@lucienn.dev>
2024-11-06 11:55:09 +0100

    fix: :bug: pass bio to formatTwiddle

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -46,6 +46,7 @@ export const formatTwiddle = async (
 			? t.likes.some(({ profileId }: { profileId: string }) => profileId === currentUserId)
 			: false,
 		author: {
+			bio: t.bio,
 			id: t.author.id,
 			role: t.author.role,
 			handle: t.author.handle,
