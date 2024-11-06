62b27797b1590fcc4e2d5bccdaa79abfa92fe871
lucien-n <contact@lucienn.dev>
2024-11-06 17:19:49 +0100

    feat: :sparkles: seed prod script

---
+++ prisma/seed-prod.ts
@@ -0,0 +1,41 @@
+import { MaintenanceMode, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const HANDLES_BLACKLIST: string[] = [
+	'settings',
+	'sign-in',
+	'sign-up',
+	'sitemaps.xml',
+	'verify',
+	'actions',
+	'api',
+	'healthz'
+];
+
+async function seedSiteSettings() {
+	console.log('seeding site settings');
+
+	const siteSettings = await prisma.siteSettings.findFirst();
+	if (siteSettings) return;
+
+	await prisma.siteSettings.create({ data: { maintenanceMode: MaintenanceMode.OPEN } });
+}
+
+async function seedHandleBlacklist() {
+	console.log('seeding handle blacklist');
+
+	const blacklistedHandles = (await prisma.handleBlacklist.findMany()).map(({ handle }) => handle);
+	const missingHandles = HANDLES_BLACKLIST.filter((handle) => !blacklistedHandles.includes(handle));
+
+	if (!missingHandles.length) return;
+
+	await prisma.handleBlacklist.createMany({
+		data: missingHandles.map((missingHandle) => ({ handle: missingHandle }))
+	});
+}
+
+await (async function () {
+	await seedSiteSettings();
+	await seedHandleBlacklist();
+})();
