eba9ae01a814d9c4734ed2a2f0f680b26cba0949
lucien-n <contact@lucienn.dev>
2024-11-14 09:55:44 +0100

    fix: :white_check_mark: use env.test

--- src/lib/server/mailjet.ts
+++ src/lib/server/mailjet.ts
@@ -1,11 +1,7 @@
 import { MJ_APIKEY_PRIVATE, MJ_APIKEY_PUBLIC } from '$env/static/private';
 import Mailjet from 'node-mailjet';
 
-export const mailjet = new Mailjet(
-	process.env.NODE_ENV === 'test'
-		? { apiKey: 'test', apiSecret: 'test' }
-		: {
-				apiKey: MJ_APIKEY_PUBLIC,
-				apiSecret: MJ_APIKEY_PRIVATE
-			}
-);
+export const mailjet = new Mailjet({
+	apiKey: MJ_APIKEY_PUBLIC,
+	apiSecret: MJ_APIKEY_PRIVATE
+});
--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,9 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient(
-	process.env.NODE_ENV === 'test'
-		? {
-				datasourceUrl: 'dont-touch-my-database'
-			}
-		: undefined
-);
+export const prisma = new PrismaClient();
