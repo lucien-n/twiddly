7111490604d07f4b8c43e401c02360f7ed151a73
lucien-n <contact@lucienn.dev>
2024-11-14 09:01:31 +0100

    ref: test structure

--- src/lib/server/__tests__/email.test.ts
+++ src/lib/server/__tests__/email.test.ts
@@ -7,81 +7,84 @@ vi.mock('$app/environment', () => ({
 	dev: false
 }));
 
-describe('email', () => {
+describe('sendEmail', () => {
 	beforeEach(() => {
 		vi.resetAllMocks();
 		mMailjet.post.mockReturnValue({ request: mMailjetPostRequest });
 	});
 
-	describe('sendEmail', () => {
-		it('should send email', async () => {
-			const mName = 'John Doe';
-			const mEmail = 'john.doe@mail.com';
-			const mBody = 'body';
-			const mSubject = 'subject';
+	it('should send email', async () => {
+		const mName = 'John Doe';
+		const mEmail = 'john.doe@mail.com';
+		const mBody = 'body';
+		const mSubject = 'subject';
 
-			await sendEmail(mBody, mSubject, { name: mName, email: mEmail });
+		await sendEmail(mBody, mSubject, { name: mName, email: mEmail });
 
-			expect(mMailjet.post).toHaveBeenCalledTimes(1);
-			expect(mMailjet.post).toHaveBeenCalledWith('send', {
-				version: 'v3.1'
-			});
-			expect(mMailjet.post).toHaveReturnedWith({ request: expect.any(Function) });
-			expect(mMailjetPostRequest).toHaveBeenCalledTimes(1);
-			expect(mMailjetPostRequest).toHaveBeenCalledWith({
-				Messages: [
-					{
-						From: {
-							Email: NO_REPLY_EMAIL,
-							Name: 'Twiddly'
-						},
-						HTMLPart: mBody,
-						Subject: mSubject,
-						To: [
-							{
-								Email: mEmail,
-								Name: mName
-							}
-						]
-					}
-				]
-			});
+		expect(mMailjet.post).toHaveBeenCalledTimes(1);
+		expect(mMailjet.post).toHaveBeenCalledWith('send', {
+			version: 'v3.1'
+		});
+		expect(mMailjet.post).toHaveReturnedWith({ request: expect.any(Function) });
+		expect(mMailjetPostRequest).toHaveBeenCalledTimes(1);
+		expect(mMailjetPostRequest).toHaveBeenCalledWith({
+			Messages: [
+				{
+					From: {
+						Email: NO_REPLY_EMAIL,
+						Name: 'Twiddly'
+					},
+					HTMLPart: mBody,
+					Subject: mSubject,
+					To: [
+						{
+							Email: mEmail,
+							Name: mName
+						}
+					]
+				}
+			]
 		});
 	});
+});
 
-	describe('sendOTPVerificationEmail', () => {
-		it('should send otp verification email', async () => {
-			mMailjetPostRequest.mockResolvedValueOnce({ response: { status: 200 } });
+describe('sendOTPVerificationEmail', () => {
+	beforeEach(() => {
+		vi.resetAllMocks();
+		mMailjet.post.mockReturnValue({ request: mMailjetPostRequest });
+	});
 
-			const mCode = '123-AZE';
-			const mName = 'John Doe';
-			const mEmail = 'john.doe@mail.com';
+	it('should send otp verification email', async () => {
+		mMailjetPostRequest.mockResolvedValueOnce({ response: { status: 200 } });
 
-			await sendOTPVerificationEmail(mCode, { name: mName, email: mEmail });
+		const mCode = '123-AZE';
+		const mName = 'John Doe';
+		const mEmail = 'john.doe@mail.com';
 
-			expect(mMailjet.post).toHaveBeenCalledTimes(1);
-			expect(mMailjet.post).toHaveBeenCalledWith('send', {
-				version: 'v3.1'
-			});
-			expect(mMailjetPostRequest).toHaveBeenCalledTimes(1);
-			expect(mMailjetPostRequest).toHaveBeenCalledWith({
-				Messages: [
-					{
-						From: {
-							Email: NO_REPLY_EMAIL,
-							Name: 'Twiddly'
-						},
-						HTMLPart: expect.any(String),
-						Subject: 'Your verification code',
-						To: [
-							{
-								Email: mEmail,
-								Name: mName
-							}
-						]
-					}
-				]
-			});
+		await sendOTPVerificationEmail(mCode, { name: mName, email: mEmail });
+
+		expect(mMailjet.post).toHaveBeenCalledTimes(1);
+		expect(mMailjet.post).toHaveBeenCalledWith('send', {
+			version: 'v3.1'
+		});
+		expect(mMailjetPostRequest).toHaveBeenCalledTimes(1);
+		expect(mMailjetPostRequest).toHaveBeenCalledWith({
+			Messages: [
+				{
+					From: {
+						Email: NO_REPLY_EMAIL,
+						Name: 'Twiddly'
+					},
+					HTMLPart: expect.any(String),
+					Subject: 'Your verification code',
+					To: [
+						{
+							Email: mEmail,
+							Name: mName
+						}
+					]
+				}
+			]
 		});
 	});
 });
