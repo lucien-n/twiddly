e2b849dea69a005ec4fd0068b48fc2c0dc82f765
Lucien <lucien.neuhoff@protonmail.com>
2024-11-19 14:53:17 +0100

    feat: :sparkles: og site_name and description

--- src/lib/components/page-infos/page-infos.svelte
+++ src/lib/components/page-infos/page-infos.svelte
@@ -3,15 +3,16 @@
 	import { PUBLIC_ORIGIN } from '$env/static/public';
 
 	const infos = $derived($page.data.infos);
+
+	const description = $derived(infos?.description ?? 'Twiddly - Share and connect with your peers');
 </script>
 
 <svelte:head>
 	<title>{infos?.title ?? 'Twiddly'}</title>
 	<meta name="author" content={infos?.author ?? 'Twiddly'} />
-	<meta
-		name="description"
-		content={infos?.description ?? 'Twiddly - Share and connect with your peers'}
-	/>
+	<meta name="description" content={description} />
+	<meta name="og:description" content={description} />
+	<meta property="og:site_name" content="Twiddly" />
 	<meta property="og:title" content={infos?.title ?? 'Twiddly'} />
 	<meta property="og:image" content={infos?.image ?? `${PUBLIC_ORIGIN}/favicon.png`} />
 	<meta property="og:image:type" content={infos?.imageType ?? 'image/png'} />
