73dc8f605d31773d535ad8d2a5970af6053f95b2
Lucien <contact@lucienn.dev>
2024-11-24 22:54:50 +0100

    feat: :lipstick: primary color for navigation progress bar

--- src/lib/sections/nav/navigation-progress-bar.svelte
+++ src/lib/sections/nav/navigation-progress-bar.svelte
@@ -6,7 +6,7 @@
 {#if $navigating}
 	<div class="absolute z-[9] w-full" transition:fly={{ y: -100, duration: 100 }}>
 		<div class="h-1.5 w-full overflow-hidden bg-secondary">
-			<div class="progress left-right h-full w-full bg-secondary-foreground"></div>
+			<div class="progress left-right h-full w-full bg-primary"></div>
 		</div>
 	</div>
 {/if}
